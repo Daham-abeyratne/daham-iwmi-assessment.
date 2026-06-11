65cb9d557042009b9b2648af4fd91fc2f9ceb773
Daham Abeyratne <Dahamabeyratney@gmail.com>
2026-06-12 02:01:42 +0530

    added basic structure of preprocessing class

---
+++ src/preprocessing.py
@@ -0,0 +1,21 @@
+class Preprocessing:
+    def __init__(self):
+        pass
+
+    def import_dataset(self):  #to load dataset and to store class names
+        pass
+
+    def inspect_dataset(self): #print class count and dataset size
+        pass
+
+    def transforms(self):  # to define train transforms and validation and train transforms
+        pass
+
+    def split_dataset(self): # to split the dataset between train/validation and test
+        pass
+
+    def create_dataloaders(self): # wrap the splits
+        pass
+
+    def visualize(self):  # to visualize transformed images
+        pass
